5c3ffdfa045bd9d56a10536e63543ec648bf70b0
Radiance Babajide <bnierimi@gmail.com>
2023-11-13 20:55:48 +0100

    Still at @g004 not @g005

--- g004/main.go
+++ g004/main.go
@@ -1,9 +1,23 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
 	// Runes
-	var char rune = 'a';
-	fmt.Println(char) 
+	var char rune = 'a'
+	fmt.Println(char)
+
+	// Strings
+	stringSlice := []string{"G", "o", "o", "d", "n", "e", "w", "s"}
+
+	var stringBuilder strings.Builder
+	for s := range stringSlice {
+		stringBuilder.WriteString(stringSlice[s])
+	}
+
+	catString := stringBuilder.String()
+	fmt.Printf("\n%v", catString)
 }
