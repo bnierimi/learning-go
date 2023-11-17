2145481e1f79c2f3af54ec7824a7d660a9770dc1
Radiance Babajide <bnierimi@gmail.com>
2023-11-18 00:41:59 +0100

    Update main.go

--- g011/main.go
+++ g011/main.go
@@ -1,19 +1,38 @@
+// func main() {
+//     fmt.Println("input text:")
+//     scanner := bufio.NewScanner(os.Stdin)
+//     scanner.Scan()
+//     err := scanner.Err()
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//     fmt.Printf("read line: %s-\n", scanner.Text())
+// }
+
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
+func input(text *string) {
+	// Read line of input: Getting user's input
+	var scanner = bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	var err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("You entered: %v\n", scanner.Text())
+	*text = scanner.Text()
+}
+
 func main() {
-    fmt.Println("input text:")
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    err := scanner.Err()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("read line: %s-\n", scanner.Text())
+	var response string
+	fmt.Printf("Enter text: ")
+	input(&response)
+	fmt.Printf("Response: %v", response)
 }
