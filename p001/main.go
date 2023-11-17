f7ffde0973bd8ee850b5877fd226eb3ff6d52e92
Radiance Babajide <bnierimi@gmail.com>
2023-11-17 20:31:31 +0100

    Update main.go

--- p001/main.go
+++ p001/main.go
@@ -30,6 +30,9 @@ func main() {
 	var age uint
 	fmt.Scan(&age)
 
+	var score int
+	var no_questions int
+
 	if age >= 10 {
 		fmt.Println("Get excited!")
 	} else {
@@ -38,14 +41,18 @@ func main() {
 	}
 
 	fmt.Printf("Which is better, [O]ne Piece or [T]okyo Ghoul? ")
+	no_questions++
 	var answer string
 	fmt.Scan(&answer)
 
-	if answer == "O" {
-		fmt.Println(":) Correct!")
-	} else if answer == "o" {
+	if answer == "O" || answer == "o" {
+		score++
 		fmt.Println(":) Correct!")
 	} else {
 		fmt.Println(":( Wrong!")
 	}
+
+	fmt.Printf("Number of Questions: %v\n", no_questions)
+	var percentage_score = (float64(score) / float64(no_questions)) * 100
+	fmt.Printf("Score: %v | %v%%", score, percentage_score)
 }
